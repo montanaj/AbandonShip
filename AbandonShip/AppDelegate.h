a57978c10b3add197fb27440d25d638c0e33d1c5
montanaj <clairejencks@gmail.com>
2014-04-30 16:15:37 -0500

    Initial Commit

---
+++ AbandonShip/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AbandonShip
+//
+//  Created by Claire on 4/30/14.
+//  Copyright (c) 2014 Jencks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AbandonShip/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AbandonShip
+//
+//  Created by Claire on 4/30/14.
+//  Copyright (c) 2014 Jencks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
